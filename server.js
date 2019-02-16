e7f411ba921d70ef4b771a6b851b0964da068e0f
zengqingbo <zengqingbo@139.com>
2019-02-16 22:14:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns=require("dns");
 
 var cors = require('cors');
 
@@ -18,6 +20,7 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,6 +34,42 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// mongodb 
+const urlSchema = mongoose.Schema({
+  url : {type: String, required: true}
+});
+
+const UrlModel = mongoose.model("Url",urlSchema);
+
+const timeout = 10000;
+
+app.post("/api/shorturl/new", (req, res, next) => {
+  let t = setTimeout(() => { next({message: 'timeout'}) }, timeout);
+  let postURL = new URL(req.body.url)
+  dns.lookup(postURL.host, (err,address,family)=>{
+    clearTimeout(t);
+    if (err) {
+      res.json({"error":"invalid URL"});
+    }
+    else {
+      let url = new UrlModel({url: req.body.url})
+      url.save((err,data)=>{
+        if (!err) {
+          res.json({original_url: data.url, short_url: data._id});
+        }
+      });
+    }
+  })
+});
+
+app.get("/api/shorturl/:id", (req, res, next) => {
+  let t = setTimeout(()=> next({message: 'timeout'}), timeout);
+  UrlModel.findById(req.params.id, (err,url) => {
+    if (url) {
+      res.redirect(url.url);
+    }
+  });
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
